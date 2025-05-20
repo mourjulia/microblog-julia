af7e7e24cc5eff784b776a0fd42f46b6d5a14643
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-05-20 09:52:40 -0300

    Adiciona iniciarSessao e exigirLogin

--- src/Auth/ControleDeAcesso.php
+++ src/Auth/ControleDeAcesso.php
@@ -10,4 +10,29 @@ Exemplo: áreas administratriva, painel de controle/dashboard, área do
 cliente, área do aluno etc.
 
 Nestas áreas o acesso se dá através de alguma forma de autenticação. 
-Exemplos, biometria, facil, 2-fatores etc. *\
+Exemplos, biometria, facil, 2-fatores etc. */
+
+final class ControleDeAcesso {
+
+    private function __construct() {}
+
+    // inicia uma sessão caso não tenha nenhuma em andamento
+    private static function iniciarSessao():void
+    {
+        if (!isset($_SESSION)) session_start();
+    }
+
+    // "Bloqueia" ´páginas admin casoo usuário NÃO ESTEJA logado
+    public function exigirLogin(): void
+    {
+        // Chama inicarSessao para iniciar (se necessário)
+        self::iniciarSessao();
+
+        // Se NÃO EXISTIR uma variável de sessão chamada ID
+        if(!isset($_SESSION['id'])){
+            session_destroy();
+            header("location:../login.php?acesso_proibido");
+            exit;
+        }
+    }
+}
